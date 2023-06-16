7ee4f488fcc049b18ffa9d714e3f41630a5ecf2f
Omar Flores <oafo2699@gmail.com>
2023-06-15 22:20:39 -0500

    añadimos botones y funcionalidades principales

--- model_test.py
+++ model_test.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import requests
+import io
 from io import BytesIO
 import cv2
 import urllib.request
@@ -8,13 +9,30 @@ import numpy as np
 import tensorflow as tf 
 
 
-def categorizar(url):
-    respuesta = respuesta.get(url)
+def categorizar(url,modelo):
+    respuesta = requests.get(url)
     img = Image.open(BytesIO(respuesta.content))
     img = np.array(img).astype(float)/255
 
     img = cv2.resize(img,(224,224))
+    prediccion = modelo.predict(img.reshape(-1,224,224,3))
+    return np.argmax(prediccion[0],axis=-1)
 
 if __name__ == "__main__":
     modelo = tf.keras.models.load_model('./Modelo_Guardado')
-    modelo.summary()  
+    #modelo.summary()
+    url = 'https://cdn.mos.cms.futurecdn.net/xwk66FPAKm63fXsgJSoucn-1200-80.jpg' 
+    response = requests.get(url)
+    imagen_data = response.content
+    imagen = Image.open(io.BytesIO(imagen_data))
+    prediccion = categorizar(url,modelo)
+    if prediccion == 0:
+        plt.imshow(imagen)
+        plt.axis('off')
+        plt.title("Detectado como Real")
+        plt.show()
+    else:
+        plt.imshow(imagen)
+        plt.axis('off')
+        plt.title("Detectado como DeepFake")
+        plt.show()
